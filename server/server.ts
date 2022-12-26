0c0c1482d866c9d200099573b2ac546a87d6aa8f
bogus <qazxwd44@hanmail.net>
2022-12-26 15:52:41 +0900

    test 로직 제거

--- server/server.ts
+++ server/server.ts
@@ -2,20 +2,4 @@ import express, { Request, Response } from 'express';
 
 const server = express();
 
-type Data = {
-  name: string;
-  age: number;
-  url: string;
-};
-
-const sendData: Data = {
-  name: 'name',
-  age: 3,
-  url: 'tistory.com',
-};
-
-server.get('/get', (req: Request, res: Response) => {
-  res.send(sendData);
-});
-
 server.listen(8080);
