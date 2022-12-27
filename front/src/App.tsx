2414a3800279a94b620ab3218c25b368df22515a
MOS <msh9900@naver.com>
2022-12-27 09:54:39 +0900

    Revert "Merge pull request #1 from msh9900/bogus"
    This reverts commit b140a6daf6510d832c3db1d1a51fcee72a1b2dc9, reversing
    changes made to 3e2b5a12228ac6c3c74488bf534722f2aa679d18.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,7 +5,6 @@ import HomePage from "./pages/HomePage";
 import IntroducePage from "./pages/IntroducePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Header from "./components/layout/Header";
-import Login from "./pages/Login/Login";
 import LoginPage from "./pages/LoginPage";
 import Profile from "./pages/Profile";
 
--- front/src/pages/Login/LoginForm.tsx
+++
@@ -1,3 +0,0 @@
-export default function LoginForm() {
-  return <div>LoginForm</div>;
-}
