f70181f46bc7713d63474e4c5d6f4bcf08f529ec
bogus <qazxwd44@hanmail.net>
2022-12-28 09:44:02 +0900

    로그인 데이터 전달까지 구현

--- front/src/pages/login/LoginPage.tsx
+++ front/src/pages/login/LoginPage.tsx
@@ -1,5 +1,30 @@
-import classes from "./LoginPage.module.scss";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import LoginForm from "../../components/LoginForm";
 
 export default function LoginPage() {
-  return <div className={classes.logo}>LoginPage</div>;
+  const [loginData, setLoginData] = useState("");
+  const navigate = useNavigate();
+
+  const findHandler = async (data: any) => {
+    const response = await fetch("http://localhost:4000/login", {
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const responseData = await response.json();
+    if (!response.ok) {
+      throw new Error("통신 에러");
+    }
+    if (responseData) {
+      setLoginData(responseData);
+    }
+  };
+
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,24 @@
-import express, { Request, Response } from 'express';
-
+const express = require('express');
+const cors = require('cors');
 const server = express();
 
-server.listen(8080);
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+// dotenv
+require('dotenv').config();
+
+//Router
+const loginRouter = require('./routes/login');
+
+server.use('/login', loginRouter);
+
+// ERROR 처리
+server.use((err: any, req: any, res: any, next: any) => {
+  console.log(err.stack);
+  res.status(err.statusCode || 500);
+  res.send(err.message);
+});
+server.listen(4000, (req: any, res: any) => {
+  console.log('4000으로 연결완료');
+});
