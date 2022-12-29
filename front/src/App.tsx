bd08d1d0ed9cb0ab457294dfbf19d912e17cb9ce
MOS <msh9900@naver.com>
2022-12-29 17:29:46 +0900

    메인페이지
    메인페이지 UI 구현  기능은 (미완성)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,4 @@
+import classes from "./App.module.scss";
 import { Routes, Route, useLocation } from "react-router-dom";
 import GuestRecruitmentPage from "./pages/guest/GuestRecruitmentPage";
 import GymRental from "./pages/rental/GymRental";
@@ -11,14 +12,11 @@ import Service from "./pages/service/Service";
 import { useEffect, useState } from "react";
 import Footer from "./components/layout/Footer";
 import Register from "./pages/login/RegisterPage";
-import { useSelector } from "react-redux";
 
 function App() {
   const location = useLocation();
-  const loginState = useSelector((state: any) => state.login.islogin);
   const [headerDel, setHeaderDel] = useState(true);
 
-  console.log(loginState);
   useEffect(() => {
     location.pathname === "/login" || location.pathname === "/register"
       ? setHeaderDel(false)
@@ -26,23 +24,25 @@ function App() {
   }, [location.pathname]);
 
   return (
-    <>
-      {headerDel ? <Header /> : ""}
-      {headerDel ? <div style={{ height: 100 }} /> : ""}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/intro" element={<IntroducePage />} />
-        <Route path="/guest" element={<GuestRecruitmentPage />} />
-        <Route path="/gym" element={<GymRental />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+    <div className={classes.wrapper}>
+      <div className={classes.contentWrapper}>
+        {headerDel ? <Header /> : ""}
+        {headerDel ? <div style={{ height: 100 }} /> : ""}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/intro" element={<IntroducePage />} />
+          <Route path="/guest" element={<GuestRecruitmentPage />} />
+          <Route path="/gym" element={<GymRental />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
 
       <Footer />
-    </>
+    </div>
   );
 }
 
--- front/src/components/layout/Footer.tsx
+++ front/src/components/layout/Footer.tsx
@@ -1,14 +1,30 @@
 import classes from "./Footer.module.scss";
-
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer>
       <div>
         <ul className={classes.menus}>
-          <li className={classes.menu}>사업자 정보</li>
-          <li className={classes.menu}>이용약관</li>
-          <li className={classes.menu}>개인정보처리방침</li>
-          <li className={classes.menu}>고객센터</li>
+          <li className={classes.menu}>
+            <Link to="/" className={classes.font}>
+              사업자 정보
+            </Link>
+          </li>
+          <li className={classes.menu}>
+            <Link to="/" className={classes.font}>
+              이용약관
+            </Link>
+          </li>
+          <li className={classes.menu}>
+            <Link to="/" className={classes.font}>
+              개인정보처리방침
+            </Link>
+          </li>
+          <li className={classes.menu}>
+            <Link to="/" className={classes.font}>
+              고객센터
+            </Link>
+          </li>
         </ul>
       </div>
       <div className={classes.copyright}>
