be7a951855ad4363a74dcd5b028f91b925b6a9fa
bogus <qazxwd44@hanmail.net>
2023-01-04 22:32:39 +0900

    multer 이미지 전송 기능 구현

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,9 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(cors());
 
+server.set('views', 'views'); // 공식화
+server.use('/images', express.static('images'));
+
 // dotenv
 require('dotenv').config();
 
@@ -14,10 +17,12 @@ require('dotenv').config();
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
 const boardRouter = require('./routes/board');
+const profileRouter = require('./routes/profile');
 
 server.use('/login', loginRouter);
 server.use('/register', registerRouter);
 server.use('/board', boardRouter);
+server.use('/profile', profileRouter);
 
 // ERROR 처리
 server.use((err: any, req: Request, res: Response, next: NextFunction) => {
