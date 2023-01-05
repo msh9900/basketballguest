a4f881ac7b315d114b8d3ca7393936217ccde995
윤시준 <webcodur@gmail.com>
2023-01-05 21:07:13 +0900

    [TEST] image uploading test

--- front/src/components/rental/Rental.tsx
+++ front/src/components/rental/Rental.tsx
@@ -5,7 +5,7 @@ import Filter from './filter/Filter'
 import Order from './order/Order'
 
 import { useState} from 'react';
-import classes from "./Rental.module.scss";
+import cls from "./Rental.module.scss";
 
 const Rental = () => {
 
@@ -17,17 +17,53 @@ const Rental = () => {
     if(isEditFormOn) setIsEditFormOn(false)
   }
 
+  const [inputImgs, setInputImgs] = useState<string[]>([])
+  const deleteImgs = () => {setInputImgs([])}
+
+  const handleImageUpload = (e:any) => {
+    const fileArr = e.target.files;
+    let fileURLs : string[] = [];
+    let file;
+    let filesLength = fileArr.length > 10 ? 10 : fileArr.length;
+
+    for (let i = 0; i < filesLength; i++) {
+      file = fileArr[i];
+      let reader = new FileReader();
+      reader.onload = () => {
+        fileURLs[i] = reader.result as string;
+        setInputImgs([...fileURLs]);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (<>
-    <div className={classes.rentalCompLayout}>
-      <button className={classes.postButton} onClick={loadEditForm}>글쓰기</button>
+    <div className={cls.rentalCompLayout}>
+      <button className={cls.postButton} onClick={loadEditForm}>글쓰기</button>
       {isEditFormOn && <RentalForm setIsEditFormOn={setIsEditFormOn}/>}
-      <Order/>
-      <Filter/>
-      <Articles/>
+      
+      <div className={cls.imageUploadBox}>
+        <input 
+          type="file"
+          multiple
+          // style={{ display: "none" }}
+          id="photo_file"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
+        {inputImgs?.map((v,i) =>
+          <img key={i} src={v} alt='profileImg1' width='100'></img>
+        )}
+      </div>
+      <button onClick={deleteImgs}>
+        이미지 삭제
+      </button>
+    
+      {/* <Order/> */}
+      {/* <Filter/> */}
+      {/* <Articles/> */}
     </div>
   </>)
-
-
 }
 
 export default Rental
