2305285f69c17fde3813a1580bdb8fa0f78246ed
MOS <msh9900@naver.com>
2023-01-13 20:45:20 +0900

    next.js 추가 세팅

--- front/components/layout/Footer.tsx
+++ front/components/layout/Footer.tsx
@@ -1,27 +1,27 @@
 import classes from "./Footer.module.scss";
-import { Link } from "react-router-dom";
+import Link from "next/link";
 export default function Footer() {
   return (
-    <footer>
+    <div className={classes.footer}>
       <div>
         <ul className={classes.menus}>
           <li className={classes.menu}>
-            <Link to="/" className={classes.font}>
+            <Link href="/" className={classes.font}>
               사업자 정보
             </Link>
           </li>
           <li className={classes.menu}>
-            <Link to="/" className={classes.font}>
+            <Link href="/" className={classes.font}>
               이용약관
             </Link>
           </li>
           <li className={classes.menu}>
-            <Link to="/" className={classes.font}>
+            <Link href="/" className={classes.font}>
               개인정보처리방침
             </Link>
           </li>
           <li className={classes.menu}>
-            <Link to="/" className={classes.font}>
+            <Link href="/" className={classes.font}>
               고객센터
             </Link>
           </li>
@@ -30,6 +30,6 @@ export default function Footer() {
       <div className={classes.copyright}>
         ⓒ 2023 MOS Company. All Rights Reserved.
       </div>
-    </footer>
+    </div>
   );
 }
--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,41 +1,31 @@
-// import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-// 가져온 값
-// style
-// import classes from "./App.module.scss";
-// react
-// import { Routes, Route, useLocation } from "react-router-dom";
-
-// You can use useRouter from 'next/router' and then you can either use pathname or asPath and then you conditionally add slug or params. Rest you can find more information via documentation posted by others.
-
 import { useEffect, useState } from "react";
-// components
-// import Footer from "../components/layout/Footer";
-// import Header from "../components/layout/header/Header";
 
+//MUI
+import CssBaseline from "@mui/material/CssBaseline";
+// components
+import classes from "./App.module.scss";
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/header/Header";
+import { Provider } from "react-redux";
+import store from "../redux/index";
 export default function App({ Component, pageProps }: AppProps) {
-  // const location = useLocation();
-  // const [headerDel, setHeaderDel] = useState(true);
-  // const [footerDel, setFooterDel] = useState(true);
-  // useEffect(() => {
-  //   location.pathname === "/login" || location.pathname === "/register"
-  //     ? setHeaderDel(false)
-  //     : setHeaderDel(true);
-  //   location.pathname === "/guest" ? setFooterDel(false) : setFooterDel(true);
-  // }, [location.pathname]);
+  const [headerDel, setHeaderDel] = useState(true);
+  const [footerDel, setFooterDel] = useState(true);
 
   return (
     <>
-      <Component {...pageProps} />
-
-      {/* <div className={classes.wrapper}>
-        <div className={classes.contentWrapper}>
-          {headerDel ? <Header /> : ""}
+      <Provider store={store}>
+        <CssBaseline />
+        <Header />
+        <div className={classes.wrapper}>
+          <div className={classes.contentWrapper}>
+            <Component {...pageProps} />
+          </div>
+          <Footer />
         </div>
-        
-        {footerDel ? <Footer /> : ""}
-      </div> */}
+      </Provider>
     </>
   );
 }
