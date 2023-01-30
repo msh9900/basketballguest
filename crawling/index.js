c55c496c6cc273647cce592f0b7552ba288ae775
윤시준 <webcodur@gmail.com>
2023-01-30 13:55:06 +0900

    [REFACT] deleted not using files

--- crawling/index.js
+++
@@ -1,7 +0,0 @@
-// // 크롤링 테스트
-// import test from "./test";
-// const test = require("./test");
-
-// const address = "서울 토성로 38-6";
-// const crawlingResult = test(address);
-// console.log(crawlingResult);
--- crawling/simple.js
+++
@@ -1,8 +0,0 @@
-
-const value = 1;
-
-const func = (v) => {
-  console.log(v)
-}
-
-func(value)
