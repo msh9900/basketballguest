98e5babdd588ab4be33fffaab78ded2b257dcf5a
bogus <qazxwd44@hanmail.net>
2023-01-31 02:49:02 +0900

    오류 수정

--- front/components/gym/mainArticles/AllArticles.tsx
+++ front/components/gym/mainArticles/AllArticles.tsx
@@ -2,25 +2,66 @@ import cls from "./AllArticles.module.scss";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import gymArticleDataType from "util/types/gymArticleDataType";
-import Image from "next/image";
 // import gymArticleDataBase from 'util/types/gymArticleDataBase';
 
-const AllArticles = () => {
+interface Props {
+  orderStatus: any;
+  filterStatus: any;
+  searchRes: any;
+  needToSearch: any;
+  setNeedToSearch: any;
+}
+const AllArticles = (props: Props) => {
   const [articles, setArticles] = useState<gymArticleDataType[]>([]);
 
   useEffect(() => {
-    getArticleData();
-  }, []);
+    // console.log("props.needToSearch", props.needToSearch);
+    if (props.needToSearch) {
+      const keyWord = props.searchRes;
+      getArticleData(keyWord);
+    }
+  }, [props.needToSearch]);
 
-  const getArticleData = async () => {
-    const response = await fetch("http://localhost:4000/rental/articles");
-    const data = await response.json()!;
-    setArticles(data);
+  const getArticleData = async (keyWord: string) => {
+    let res: any;
+
+    // 필터링한 검색
+    if (keyWord !== "") {
+      const body = {
+        filter: props.filterStatus,
+        order: props.orderStatus,
+        keyWord: keyWord,
+      };
+
+      try {
+        const response = await fetch("http://localhost:4000/rental/search", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        });
+        res = await response.json();
+        console.log("검색 결과 : ", res);
+      } catch (err: any) {
+        console.log("필터,정렬 적용한 검색 실패 : ", err);
+      }
+    }
+
+    // 전체 검색
+    else {
+      try {
+        const response = await fetch("http://localhost:4000/rental/articles");
+        res = await response.json();
+        console.log("검색 결과 : ", res);
+      } catch (err: any) {
+        console.log("기본 검색 실패 : ", err);
+      }
+    }
+    props.setNeedToSearch(false);
+    setArticles(res);
   };
 
   const router = useRouter();
   const moveToDetailPage = (num: string) => {
-
     router.push(`/gym/${num}/`);
   };
 
@@ -38,15 +79,7 @@ const AllArticles = () => {
                 }}
               >
                 <li className={cls.li}>
-                  <div className={cls.imgBox}>
-                    <img src={item.gymImg[0]} />
-                    {/* <Image
-                      src={item.userImg[0]}
-                      width="200"
-                      height="100"
-                      alt=""
-                    /> */}
-                  </div>
+                  <div className={cls.imgBox}></div>
                   <div className={cls.title}>제목 : {item.title}</div>
                   <div className={cls.price}>대관료 : {item.price}원/시간</div>
                   <div className={cls.content}>내용 : {item.content}</div>
