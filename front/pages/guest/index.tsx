506856bbc1cfbba2d1b43b00fb41bac79e22d6d0
bogus <qazxwd44@hanmail.net>
2023-01-31 03:16:37 +0900

    오류 수정

--- front/pages/guest/index.tsx
+++ front/pages/guest/index.tsx
@@ -1,96 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import path from "path";
+import fs from "fs/promises";
 import Card from "../../components/recruit/Card";
 import classes from "./guest.module.scss";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Modal from "@mui/material/Modal";
 import WriteModal from "../../components/recruit/WriteModal";
-export default function GuestRecruitmentPage() {
-  //임시 데이터 카드
-  const data = [
-    {
-      contentidx: 1,
-      id: "테스트1",
-      title: "[대구] 농구 게스트 구합니다",
-      content: "안녕하세요 체육관 대여 할려고 하는데 비용이 얼마정도 나오나요?",
-      image: "src주소 1",
-      date: "1시간전",
-      comment: [
-        {
-          commentidx: 1,
-          id: "test1",
-          content: "안녕하세요 게스트 구하려고 하는데 몇명정도 구하시나요",
-        },
-        {
-          commentidx: 2,
-          id: "test2",
-          content: "test2 댓글 입니다",
-        },
-        {
-          commentidx: 3,
-          id: "test3",
-          content: "test3 댓글 입니다",
-        },
-      ],
-    },
-    {
-      contentidx: 2,
-      id: "테스트2",
-      title: "[이태원] 농구 게스트 구합니다",
-      content: "테스트2 입니다",
-      date: "1시간전",
-      comment: [
-        {
-          commentidx: 1,
-          id: "test1",
-          content: "안녕하세요 게스트 구하려고 하는데 몇명정도 구하시나요",
-        },
-        {
-          commentidx: 2,
-          id: "test2",
-          content: "가나다라마바사",
-        },
-        {
-          commentidx: 3,
-          id: "test3",
-          content: "test3 댓글 입니다",
-        },
-      ],
-    },
-  ];
-  for (let i = 3; i < 100; i++) {
-    data.push({
-      contentidx: i,
-      id: "테스트2",
-      title: "[이태원] 농구 게스트 구합니다",
-      content: "테스트2 입니다",
-      date: "1시간전",
-      comment: [
-        {
-          commentidx: 1,
-          id: "test1",
-          content: "안녕하세요 게스트 구하려고 하는데 몇명정도 구하시나요",
-        },
-        {
-          commentidx: 2,
-          id: "test2",
-          content: "ㅁㄴㅇㅁㄴㅇ",
-        },
-        {
-          commentidx: 3,
-          id: "test3",
-          content: "test3 ㅁㄴㅇㅇㅇ 입니다",
-        },
-      ],
-    });
-  }
-
+export default function GuestRecruitmentPage(props: any) {
+  const { data } = props;
   const [contentList, setContentList] = useState(10);
   const [hasMore, setHasMore] = useState(true);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const fetchMoreData = () => {
-    if (contentList < 150) {
+    if (contentList < data.length) {
       setTimeout(() => {
         setContentList(contentList + 10);
       }, 500);
@@ -98,6 +23,7 @@ export default function GuestRecruitmentPage() {
       setHasMore(false);
     }
   };
+
   return (
     <>
       <div className={classes.writeButton} onClick={handleOpen}>
@@ -122,7 +48,7 @@ export default function GuestRecruitmentPage() {
           scrollableTarget="parentScrollDiv"
         >
           <div className={classes.container}>
-            {data.slice(0, contentList).map((val) => (
+            {data.slice(0, contentList).map((val: any) => (
               <Card key={val.contentidx} data={val} />
             ))}
           </div>
@@ -130,4 +56,13 @@ export default function GuestRecruitmentPage() {
       </div>
     </>
   );
+}
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const filePath = path.join(process.cwd(), "data", "guest-data.json");
+  const jsonData: any = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  // Pass data to the page via props
+  return { props: { data: data.data } };
 }
