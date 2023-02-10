a6eae0ee6ff72bf9144fae161c1e0aeb9743bf38
bogus <qazxwd44@hanmail.net>
2023-02-11 00:20:03 +0900

    crawler test

--- back/module/crawler.js
+++ back/module/crawler.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const crawler = async (address) => {
   const browser = await puppeteer.launch({
     // headless: false,
-    slowMo: 30,
+    slowMo: 10,
     // args: ['--no-sandbox', '--disable-setuid-sandbox'],
     // args: ['--window-size=1920,1080'],
   });
