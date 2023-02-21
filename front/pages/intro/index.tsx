bb400d7c061c1076bd741b269e2893c49bf670ee
bogus <qazxwd44@hanmail.net>
2023-02-21 16:23:25 +0900

    profile default img 수정

--- front/pages/intro/index.tsx
+++ front/pages/intro/index.tsx
@@ -9,15 +9,16 @@ import { CardActionArea } from "@mui/material";
 
 const data = [
   {
-    name: "모승환",
+    name: "모승환 대장",
     image: "/basketball1.jpg",
-    content: "자기소개 내용",
+    content: "자기소",
     url: "https://github.com/msh9900",
   },
   {
-    name: "모승환",
-    image: "/basketball1.jpg",
-    content: "자기소개 내용",
+    name: "천해성",
+    image: "https://avatars.githubusercontent.com/u/100825999?v=4",
+    content:
+      "로그인 기능(Redux) 구현 및 로그인 쿠키 로직 구현, 회원가입 구현, 마이페이지 구현 및 각 컴포넌트에서 데이터를 전달하기 위한 API 구현 및 데이터베이스구축 (백엔드)",
     url: "https://github.com/BoGusD",
   },
   {
